a174e222583dfe6f19dbfe2957ddcf0a57daffa0
Gaoyu Liu <Gaoyu.Liu@outlook.com>
2026-08-09 00:52:36 +0100

    v0..1.2.4

--- src/apis/user/manage.py
+++ src/apis/user/manage.py
@@ -59,8 +59,9 @@ async def post_accept(request: Request, response: Response, uid: int, authorizat
                 elif app.config.trackers[0]["type"] == "unitracker":
                     tracker_in_use = 5
             else:
-                response.status_code = 400
-                return {"error": ml.tr(request, "config_value_is_empty", var = {"item": "tracker"}, force_lang = au["language"])}
+                # A Hub can be used without TrackSim/Trucky/etc.  In that case a
+                # newly accepted member simply has no delivery tracker selected.
+                tracker_in_use = 0
     except:
         response.status_code = 400
         return {"error": ml.tr(request, "bad_json", force_lang = au["language"])}
